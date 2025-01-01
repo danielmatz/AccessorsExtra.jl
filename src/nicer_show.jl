8017473968e1404995e2b6fcc29606a34d46c0c2
Alexander Plavin <alexander@plav.in>
2025-01-01 00:00:00 +0000

    upstreamed show

--- src/nicer_show.jl
+++ src/nicer_show.jl
@@ -30,32 +30,3 @@ end
 
 _eltype(T) = eltype(T)
 _eltype(::Type{Union{}}) = Union{}
-
-
-
-# XXX: piracy, should upstream the changes
-function Accessors.show_optic(io::IO, optic)
-    opts = deopcompose(optic)
-    inner = Iterators.takewhile(x -> applicable(Accessors._shortstring, "", x), opts)
-    outer = Iterators.dropwhile(x -> applicable(Accessors._shortstring, "", x), opts)
-    if !isempty(outer)
-        show(io, opcompose(outer...))
-    end
-    if !isempty(inner) && !isempty(outer)
-        print(io, " ∘ ")
-    end
-    if !isempty(inner)
-        shortstr = reduce(inner; init=("_", false)) do (prev, need_parens_prev), o
-            # if _need_parens is true for this o and the one before, wrap the previous one in parentheses
-            if need_parens_prev && Accessors._need_parens(o)
-                prev = "($prev)"
-            end
-            Accessors._shortstring(prev, o), Accessors._need_parens(o)
-        end |> first
-        if get(io, :compact, false)
-            print(io, shortstr)
-        else
-            print(io, "(@o ", shortstr, ")")
-        end
-    end
-end
