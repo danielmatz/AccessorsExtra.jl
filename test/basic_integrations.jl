00783634fe98f2b1201170c7e469f975f10f1d9c
Alexander Plavin <alexander@plav.in>
2025-01-01 00:00:00 +0000

    comment

--- test/basic_integrations.jl
+++ test/basic_integrations.jl
@@ -29,11 +29,11 @@ end
     using StructArrays
     using StructArrays.Tables
 
+    # these setproperties() are upstreamed to StructArrays:
     s = StructArray(a=[1, 2, 3])
     @test setproperties(s, a=10:12)::StructArray == StructArray(a=10:12)
     @test_throws ArgumentError setproperties(s, b=10:12)
     @test @modify(c -> c .+ 1, s |> Properties()) == StructArray(a=[2, 3, 4])
-
     s = StructArray(([1, 2, 3],))
     @test setproperties(s, (10:12,))::StructArray == StructArray((10:12,))
     @test @modify(c -> c .+ 1, s |> Properties()) == StructArray(([2, 3, 4],))
